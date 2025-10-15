30dcb980fa9d5d37a7f4803c0bf957ab7ef9b35d
Glenn MIAKASSISSA [AC-TRANSFORMATION] <Glenn.MIAKASSISSA@larchergroup.com>
2025-10-15 15:57:30 +0100

    Initial commit: ajout app (et tests) simples

---
+++ app.js
@@ -0,0 +1,9 @@
+// app.js:
+function greet(name) {
+    if (!name) {
+        return "Hello, world!";
+    }
+    return `Hello, ${name}!`;
+}
+
+module.exports = greet;
---
+++ app.test.js
@@ -0,0 +1,9 @@
+// app.test.js:
+const greet = require('./app.js');
+test('should greet the world when no name is provided', () => {
+  expect(greet()).toBe("Hello, world!");
+});
+ 
+test('should greet a specific name', () => {
+  expect(greet('Glenn')).toBe("Hello, Glenn!");
+});
